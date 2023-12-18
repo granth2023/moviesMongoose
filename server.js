d81188b304d963eaae860312fbbcecd9020cfd91
granth2023 <grantharris38@gmail.com>
2023-12-18 11:15:05 -0500

    what next

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ import logger from 'morgan'
 import './config/database.js';
 
 //what are putting in here? are we importing our database? what is this import doing? is this the entry port of our app? why assign database module to variable? what do we need to do to make code run? what do we need dot env file? dot config? what do you think? what do you think? 
+
