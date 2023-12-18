68d7b6e5e8bcba0f7868767dc72bd2434723b22b
granth2023 <grantharris38@gmail.com>
2023-12-18 09:38:19 -0500

    middle ware number 2

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,6 @@ router.get('/', function(req, res) {
 
 // what is middleware? 
 //we process http, build routes, view engine to render dynamic templates OR send back dataa in different formats like json 
+
+//function(req, res)  is a callback function
+//next --> what is that saying? move onto the next function? 
