16554b1ba168eb8b3d9c2f6a6377f94adfe284b8
granth2023 <grantharris38@gmail.com>
2023-12-18 09:41:03 -0500

    middleware

--- routes/index.js
+++ routes/index.js
@@ -10,3 +10,4 @@ router.get('/', function(req, res) {
 //function(req, res)  is a callback function
 //next --> what is that saying? move onto the next function? 
 //what is the next funciton? what is a way for middleware to go to the next step? 
+//what do we look for in server dot js, whats the first thing we do? how do we call express into a variable? what do we add? what does the app use? what happens when a request comes in? what does it hit first? what does it do to the request? what does it add? what does next do? waht is path middleware? what is cors using? is cors using next? what happens when it calls next? what does a route do? how does it accept requests at urls? how do we runt he server? what port? 
